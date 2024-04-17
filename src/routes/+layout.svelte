cf87ccc455cdb98b82663b52e6d226ba987221a5
Albert R. González-Marín <dsifriend@gmail.com>
2024-04-17 16:14:29 +0200

    Added search bar component.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,9 @@
 	<main class="container">
 		<h1>ייִדיקט</h1>
 		<hr />
-		<form id="searchbox"></form>
+		<form id="searchbox">
+			<input type="search" name="search" placeholder="זוך" aria-label="זוך⸗פונקציע" />
+		</form>
 		<hr />
 		<slot />
 	</main>
