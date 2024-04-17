5de063745d7217d932bc877f55dcc25511caca82
Albert R. González-Marín <dsifriend@gmail.com>
2024-04-17 15:34:38 +0200

    Added base skeleton with search bar.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,16 @@
 	<link rel="stylesheet" href="css/pico.blue.min.css" />
 </svelte:head>
 
+<body>
+	<main class="container">
+		<h1>ייִדיקט</h1>
+		<hr />
+		<form id="searchbox"></form>
+		<hr />
+		<slot />
+	</main>
+</body>
+
 <style lang="scss">
 	* {
 		font-family: Arial, sans-serif;
