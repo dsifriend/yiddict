76062b9b63eb6aa9443e1b9a7a9e9f3ba0fc64b6
Albert R. González-Marín <dsifriend@gmail.com>
2024-04-17 20:09:02 +0200

    Wrapped advanced search options in a dropdown.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,17 +8,20 @@
 		<form id="searchbox">
 			<input type="search" name="search" placeholder="זוך" aria-label="זוך⸗פונקציע" />
 			<!-- Advanced Options -->
-			<fieldset>
-				<label>
-					<input name="wholeWord" type="checkbox" role="switch" />
-					גאַנצע־װערטער
-				</label>
-			</fieldset>
-			<select name="wordForm" aria-label="בייגונג">
-				<option selected disabled value="base">בייגונג</option>
-				<option value="base">געבױגן</option>
-				<option value="word">אומגעבױגן</option>
-			</select>
+			<details>
+				<summary>ברירות</summary>
+				<fieldset>
+					<label>
+						<input name="wholeWord" type="checkbox" role="switch" />
+						גאַנצע־װערטער
+					</label>
+				</fieldset>
+				<select name="wordForm" aria-label="בייגונג">
+					<option selected disabled value="base">געבױגן</option>
+					<option value="base">געבױגן</option>
+					<option value="word">אומגעבױגן</option>
+				</select>
+			</details>
 		</form>
 	</header>
 	<main>
