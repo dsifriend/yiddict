1b8ae26f8c37376b6ec74a249462496509ed656d
Albert R. González-Marín <dsifriend@gmail.com>
2024-04-19 19:18:31 +0200

    Wrote About page draft.

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,27 @@
+<main dir="ltr">
+	<h1>װעגן אונז</h1>
+	<h2>About Us</h2>
+	<p>
+		The <strong>yiddict.app</strong> project seeks to bring a convenient, easy-to-use dictionary app
+		for all speakers and learners of <em>ייִדיש</em>.
+	</p>
+	<p>
+		The content on <em>yiddict.app</em> is sourced from <strong>Raphael Finkel's</strong>
+		own
+		<a href="https://www.cs.uky.edu/~raphael/yiddish/dictionary.cgi">
+			<em>Yiddish-English</em> dictionary lookup site.</a
+		>
+		In fact, this website fetches its entry data directly from Finkel's in its current state. The motivation
+		for <em>yiddict.app</em> stemms in large part from a personal desire to improve the user
+		experience of Finkel's site <i>and</i> to update its interface to current tastes and use cases (i.e.
+		mobile web browsers).
+	</p>
+	<p>Future goals for the project include:</p>
+	<ul>
+		<li>Native apps for iOS and Android</li>
+		<li>Offline functionality</li>
+		<li>Search suggestions</li>
+		<li>Proper English-Yiddish search and entries</li>
+		<li>More data sources</li>
+	</ul>
+</main>
