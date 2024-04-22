5abb8bcdeefcdff1246a4c4f0da70b40f96a8bf3
Albert R. González-Marín <dsifriend@gmail.com>
2024-04-22 12:03:34 +0200

    Began writing help page.

---
+++ src/routes/help/+page.svelte
@@ -0,0 +1,95 @@
+<main dir="ltr">
+	<h1>הילף</h1>
+	<h2>Help</h2>
+	<p>
+		The search function might not work as expected, if you're used to more modern/complex online
+		dictionaries (for now). In general, only exact matches (whether partial or whole) are returned.
+		The following hints should help you find what you need.
+	</p>
+	<section>
+		<h3>Searchable Strings</h3>
+		<p>
+			Currently, the search function searches for matches in all text belonging to an entry (i.e.
+			term, definition, grammar notes, etc.) The Yiddish-first UI is intended to encourage input in
+			straight Yiddish, but internally all entries are encoded in their YIVO-standard transcription.
+			Entries for לשינ־קודש words should be searchable with their hebrew spelling. See notes on
+			transcription below.
+		</p>
+		<h3>Yiddish words in Yiddish letters</h3>
+		<p>
+			You may use ligatures or not, and if you leave out nekudes, the program will try to fill them
+			in.
+		</p>
+		<p>
+			Try searching for the <strong>stem of a word</strong> (not the plural, case-marked, or conjugated
+			form) of nouns, adjectives, and verbs. Unless disabled from the advanced search options, the app
+			will try identifying this for you, but it's not perfect.
+		</p>
+		<details>
+			<summary>Examples</summary>
+			<ul>
+				<li>Type <b>פּלימעניק</b>, not <i>פּלימעניקעס</i></li>
+				<li>
+					Type <b>שײן</b>, not <i>שײנע</i>, and <b>העלף</b>, not <i>העלפֿן</i> or
+					<i>אונטערגעהאָלפֿן</i>
+				</li>
+			</ul>
+		</details>
+	</section>
+	<section>
+		<h3>Yiddish words in YIVO transcription</h3>
+		<p>You may write Yiddish words using their YIVO transcription.</p>
+		<p>
+			Try searching for the <strong>stem of a word</strong> (not the plural, case-marked, or conjugated
+			form) of nouns, adjectives, and verbs. Unless disabled from the advanced search options, the app
+			will try identifying this for you, but it's not perfect.
+		</p>
+		<details>
+			<summary>Examples</summary>
+			<ul>
+				<li>Type <b>plimenik</b>, not <i>plimenikes</i></li>
+				<li>Type <b>sheyn</b>, not <i>sheyne</i></li>
+				<li>Type <b>helf</b>, not <i>helfn</i> or <i>geholfn</i></li>
+			</ul>
+		</details>
+		<details>
+			<summary>YIVO Alphabet</summary>
+			<p>YIVO uses the following <strong>consonants </strong>for its transcriptions:</p>
+			<q
+				>ב:b ג:g ד:d ה:h װ:v ז:z כ:kh ט:t י:y ל:l מ:m נ:n ס:s פֿ:f פּ:p צ:ts ק:k ר:r ש:sh טש:tsh זש:zh
+				דזש:dj</q
+			>
+			<p>YIVO uses the following <strong>vowels</strong> for its transcriptions:</p>
+			<q>אַ:a ע:e י:i אָ:o ו:u ױ:oy ײַ:ay ײ:ey</q>
+			<details>
+				<summary>Example</summary>
+				<p>The following sentence is correctly transliterated:</p>
+				<q>איך בינ געקומנ אַזױ װײַט צו שרײַבן אַ ביסל מאַמע־לשון!</q>
+				<q>ikh bin gekumen azoy vayt tsu shraybn a bisl mame-loshn!</q>
+			</details>
+			<p>
+				Do not use German phonetic spelling. For instance, don't write <i>schpiel</i> to mean
+				<b>shpil</b>.
+			</p>
+		</details>
+	</section>
+	<section>
+		<h3>Words derived from לשינ־קודש</h3>
+		<p>
+			Use capital letters for the first letters of Hebrew proper names (like <b>Refoyl</b> and
+			<b>Binyomen</b>). Don't use <em>capital letters</em> for any other Yiddish words.
+		</p>
+		<p>
+			If you need a לשינ־קודש derived word, first spell it phonetically. Unaccented vowels often
+			should be spelled with an <b>e</b>, as in <b>borekh</b>, <b>bimkem</b>, and <b>maskem</b>.
+		</p>
+		<details>
+			<summary>ASCII encoding for לשינ־קודש</summary>
+			<p>
+				If you don't know or can't guess the pronunciation (or rather, the YIVO transcription), the
+				app provides the following custom ASCII-based encoding to help input לשינ־קודש if required:
+			</p>
+			<q></q>
+		</details>
+	</section>
+</main>
