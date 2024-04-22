8150d07a349a27f3b1888651d837ffe074d7bfb6
Albert R. González-Marín <dsifriend@gmail.com>
2024-04-23 00:14:06 +0200

    Drafted License Page.

---
+++ src/routes/license/+page.svelte
@@ -0,0 +1,27 @@
+<main dir="ltr">
+	<h1>מחברים־רעכט*</h1>
+	<h2>License & Copyright (*WIP)</h2>
+	<p>
+		<em>Yiddict.App</em> is built on <a href="https://svelte.dev">Svelte</a> and styled with
+		<a href="https://picocss.com">PicoCSS</a>. Both are licensed under the
+		<a href="https://opensource.org/license/mit">MIT Software License</a>.
+	</p>
+	<p>
+		The dictionary content is sourced from the <a
+			href="https://www.cs.uky.edu/~raphael/yiddish/dictionary.cgi">dictionary lookup page</a
+		>
+		at <em>Refoyls Yidish Veb-Bletl</em> by <strong>Raphael Finkel</strong>. License to use this
+		content is <mark>UNCLEAR [MUST CONSULT]</mark>.
+	</p>
+	<p>
+		The Yiddict.App project aims to make essential learning resources like this convenient and
+		accessible to all Yiddish speakers at no cost to them and, moreover, to keep them that way. As
+		such, the software written for <strong>Yiddict.App</strong> is licensed under the
+		<a href="https://www.gnu.org/licenses/gpl-3.0.html">GNU General Public License v3</a> and its
+		source available from the <a href="#">official repo [PENDING LICENSE CONSULTATION]</a>
+		or by contacting the maintainer at:
+	</p>
+	<address style="text-align: right;">
+		<a href="mailto:editorial@albertrg.me">editorial@albertrg.me</a>
+	</address>
+</main>
