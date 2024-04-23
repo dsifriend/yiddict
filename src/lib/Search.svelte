d5cfb41c4fd2e979b4e4688f8cebc7a5ca17f321
Albert R. González-Marín <dsifriend@gmail.com>
2024-04-24 00:23:37 +0200

    Made search input required in the search form.

---
+++ src/lib/Search.svelte
@@ -0,0 +1,18 @@
+<form id="searchbox">
+	<input type="search" name="search" placeholder="זוך" aria-label="זוך⸗פונקציע" required />
+	<!-- Advanced Options -->
+	<details style="max-width: fit-content;">
+		<summary>ברירות</summary>
+		<fieldset>
+			<label>
+				<input name="wholeWord" type="checkbox" role="switch" />
+				גאַנצע־װערטער
+			</label>
+		</fieldset>
+		<select name="wordForm" aria-label="בייגונג">
+			<option selected disabled value="base">געבױגן</option>
+			<option value="base">געבױגן</option>
+			<option value="word">אומגעבױגן</option>
+		</select>
+	</details>
+</form>
