52e482f54fea1397f28bed85f0b7d982ccf49bbf
Albert R. González-Marín <dsifriend@gmail.com>
2024-05-14 01:17:20 +0200

    Reimplemented search results page using SvelteKit routing

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,20 +1,34 @@
+<script lang="ts">
+	export let data: { results: string };
+</script>
+
 <main dir="ltr">
-	<h1 dir="rtl">װאַרטען אױף זוך־רעזולטאַטן…</h1>
-	<progress />
+	{#await data.results}
+		<h2 dir="rtl">זוך אַ װאָרט, אַ ליאַדע װאָרט!</h2>
+	{:then results}
+		<h2>רעזולטאַטן</h2>
+		<br />
+		<ul>{results}</ul>
+		<br />
+		<p style="text-align: end;">
+			Sourced from
+			<a href="https://www.cs.uky.edu/~raphael/yiddish/dictionary.cgi">רפֿאלס ייִדיש וועב–בלעטל</a>
+		</p>
+	{:catch}
+		<h2>רעזולטאַטן</h2>
+		<br />
+		<h3>גאַרנישט געפֿונען</h3>
+	{/await}
 </main>
 
 <style lang="scss">
 	@use '@picocss/pico/scss/colors' as *;
 
-	:global(h2) {
-		color: $azure-500;
-	}
-
-	:global(main > ul) {
+	main > ul {
 		list-style: none;
 	}
 
-	:global(main > ul > li) {
+	main > ul > li {
 		margin-bottom: 3rem;
 	}
 </style>
---
+++ src/routes/search/+page.ts
@@ -0,0 +1,51 @@
+import * as cheerio from 'cheerio';
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = async ({ url }) => {
+	// Convert from the URLSearchParams back to FormData.
+	const q = new FormData();
+	for (const param of url.searchParams) {
+		q.append(param[0], param[1]);
+	}
+
+	return {
+		results: await getResults(q)
+	};
+};
+
+// Function to fetch and collect search results from various sources.
+async function getResults(q: FormData): Promise<string | null> {
+	// All search results are fetched from Rafael Finkel's site atm.
+	return searchFinkel(q);
+}
+
+// Post a query to Raphael Finkel's dictionary lookup.
+async function searchFinkel(q: FormData): Promise<string | null> {
+	// Construct `FormData` object compatible w/ external source.
+	const formdata = new FormData();
+	// Add the search term `search` to the corresponding field.
+	formdata.append(q.get('wordForm') as string, q.get('search') as string);
+	// Add preference for whole or substring matches.
+	if (q.has('wholeWord')) {
+		formdata.append('wholeWord', 'on');
+	}
+	// Make external POST request
+	const response = await fetch('https://www.cs.uky.edu/~raphael/yiddish/dictionary.cgi', {
+		method: 'post',
+		body: formdata
+	}).then((t) => t.text());
+
+	// Use CheerioJS to query for the search results within the `response`.
+	const $ = cheerio.load(response);
+
+	// Wrap elements to apply our own styles.
+	$('.lexeme').wrap('<strong></strong>');
+	$('.grammar').wrap('<small></small>');
+	$('.goodmatch').wrap('<ins></ins>');
+	$('.goodmatch').wrap('<mark></mark>');
+	$('.weakmatch').wrap('<mark></mark>');
+
+	const results = $('ul').html();
+
+	return results ? results : '';
+}
