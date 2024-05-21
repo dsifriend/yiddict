4f1cf05766e931b78f21f6dfd620565ec9546757
Albert R. González‑Marín <dsifriend@gmail.com>
2024-05-21 22:18:11 +0200

    Updated License

--- src/routes/license/+page.svelte
+++ src/routes/license/+page.svelte
@@ -1,5 +1,5 @@
 <main dir="ltr">
-	<h1>מחברים־רעכט*</h1>
+	<h1>מחברים־רעכט</h1>
 	<h2>License & Copyright</h2>
 	<p>
 		<em>Yiddict.App</em> is built on <a href="https://svelte.dev">Svelte</a> and styled with
@@ -7,11 +7,13 @@
 		<a href="https://opensource.org/license/mit">MIT Software License</a>.
 	</p>
 	<p>
-		Dictionary entries are sourced from the <a
-			href="https://www.cs.uky.edu/~raphael/yiddish/dictionary.cgi">dictionary lookup page</a
-		>
+		Dictionary entries are sourced from the
+		<a href="https://www.cs.uky.edu/~raphael/yiddish/dictionary.cgi">dictionary lookup page</a>
 		at <em>Refoyls Yidish Veb-Bletl</em> by <strong>Raphael Finkel</strong>. Permission to use this
 		content is governed by the
+		<a href="https://creativecommons.org/licenses/by-nc/3.0/">CC BY-NC 3.0</a>
+		license conditions. at <em>Refoyls Yidish Veb-Bletl</em> by <strong>Raphael Finkel</strong>.
+		Permission to use this content is governed by the
 		<a href="https://creativecommons.org/licenses/by-nc/3.0/">CC BY-NC 3.0</a>
 		license conditions.
 	</p>
@@ -19,6 +21,10 @@
 		The Yiddict.App project aims to make essential learning resources like this convenient and
 		accessible to all Yiddish speakers at no cost to them and, moreover, to keep them that way. As
 		such, the software written for <strong>Yiddict.App</strong> is licensed under the
+		<a href="https://www.gnu.org/licenses/gpl-3.0.html">GNU General Public License v3</a>
+		and its source is available from the
+		<a href="https://github.com/dsifriend/yiddict">official GitHub repo</a>, or by contacting the
+		maintainer at:
 		<a href="https://www.gnu.org/licenses/gpl-3.0.html">GNU General Public License v3</a>
 		and its source is available from the
 		<a href="https://github.com/dsifriend/yiddict">official GitHub repo</a>, or by contacting the
