6536d729e1a09cfb5bfc2d357583aab6b72d1da1
Albert R. González‑Marín <dsifriend@gmail.com>
2025-10-09 15:16:06 -0400

    Finished defining tokenizer for bracketed data

---
+++ packages/unifier/src/converters/refoyl.ts
@@ -0,0 +1,224 @@
+/**
+ * @fileoverview Script to convert Rafael (Refoyl) Finkel's lexicon into JSON using the OntoLex model.
+ */
+
+import {
+  Lexicon,
+  LexicalEntry,
+  LexiconBuilder,
+  LexicalEntryBuilder,
+} from "@yiddict/lexicon";
+import {
+  seq,
+  apply,
+  opt_sc,
+  rep_sc,
+  tok,
+  Parser,
+  Token,
+  buildLexer,
+} from "typescript-parsec";
+
+/**
+ * Lines in Refoyl's source file can be categorized into four broad types:
+ * - Empty lines
+ *  - Appear empty and are ignored.
+ * - Comment lines (starting with %)
+ *  - Contain comments meant for other devs or Refoyl himself, also ignored.
+ * - Entry lines (starting with a non-whitespace character)
+ *  - Contain the main lexical entries, which are processed to create `LexicalEntry` objects.
+ * - Sub-entry lines (beginning with one or more tab characters)
+ *  - Generally represent some derived form, be it provided explicitly or derived from the main entry.
+ */
+enum LineCategory {
+  Empty,
+  Comment,
+  Entry,
+  SubEntry,
+}
+const LineCategorizer = buildLexer([
+  [false, /^\s*$/gm, LineCategory.Empty],
+  [false, /^\s*%.*$/gm, LineCategory.Comment],
+  [true, /^\S.*$/gm, LineCategory.Entry],
+  [true, /^\t[^\s%].*$/gm, LineCategory.SubEntry],
+]);
+
+// Knowing whether a string is between brackets or not is crucial for picking
+// the right behavior for parsing an entry's syntax.
+/** Surrounds its input w/ regex such that it won't match if between brackets. */
+const unbracketed = (inner: string) => `(?<!\[[^\]]*)${inner}(?![^\[]*\])`;
+/** Surrounds its input w/ regex such that it will only match if between brackets. */
+const bracketed = (inner: string) => `(?<=\[)${inner}(?=\])`;
+
+/**
+ * Refoyl uses a few characters outside the usual `A-Za-z` range
+ * to encode his transliterated forms.
+ */
+const FormCharacters = `[\w'|#-]`;
+/**
+ * Transliterated forms consist of a contiguous string of `FormCharacters`.
+ */
+const FormBaseStr = `${FormCharacters}+`;
+/**
+ * Transliterated forms with a spelling hint are followed
+ * by another such string between curly braces, rarely separated
+ * by whitespace inbetween.
+ */
+const FormPatternStr = `${FormBaseStr}(?:\s*\{${FormBaseStr}\})?`;
+/** Forms to be captured only occur after whitespace outside of brackets. */
+const FormPattern = RegExp(`(?<=^|\s)${unbracketed(FormPatternStr)}`, "g");
+
+/**
+ * Refoyl defines a set of so-called "codes" that are used to encode
+ * linguistic data compactly. These codes are used in the entries
+ * and sub-entries to indicate various grammatical features or forms.
+ * The codes are typically prefixed with a slash (/) and can be followed
+ * by additional information such as endings or forms.
+ *
+ * While we might expect all word class markers to be encoded as such,
+ * they're not, instead relying on a different syntax entirely.
+ * Other "codes" are rather used to instruct a transformer to generate
+ * regular forms or perform other tasks dynamically, hence the name `MacroSymbol`.
+ */
+enum MacroSymbol {
+  // Word-class markers
+  Adjective = "/A", // Regular adjective
+  AdjectiveK = "/K", // Gradable adjective. Takes an optional irregular form as argument
+  AdjectiveI = "/I", // Takes a suffix as its argument to generate an adjective
+  Noun = "/N", // Regular noun, plural in -n
+  NounS = "/S", // Regular noun, plural in -s
+  NounX = "/X", // Irregular noun, plural takes an alternative form as its argument
+  NounProper = "/C", // Proper noun, takes the dative form
+  NounDiminutive = "/D", // Generates diminutive forms of a noun. Takes an optional argument for irregular stems.
+  Verb = "/V", // Regular verb
+  VerbT = "/T", // Regular verb with unprefixed participle
+  VerbB = "/B", // Regular verb with irregular participle
+  VerbComplement = "/G", // Generate forms with the given adverbial complement.
+  Preposition = "/P",
+  // General Markers
+  Prefix = "/L", // Indicates that the given prefix generates a valid form. Common for hebrew particles.
+  Suffix = "/E", // Indicates that the given suffix generates a valid form. Primarily used for epenthetical -e- in otherwise regular plural forms.
+  LoshnKoydesh = "/H", // Indicates a non-phonetic spelling, possibly, and usually for "loshn-koydesh" words. Superfluous with nearly no exceptions (<25)
+  Spurious = "/-", // Indicates the given form (its argument) should NOT be generated if it otherwise would be.
+}
+/**
+ * Although we define an individual token for every `MacroSymbol`,
+ * it's still convenient to group these together at a higher level.
+ */
+const MacroSymbolCharacters = "[ABCDEGHIKLNPSTVX-]";
+/**
+ * This pattern matches a `MacroSymbol` with an optional argument.
+ * It could be refined to only check for an argument after symbols
+ * intended to take one, but in practice this is fine.
+ */
+const MacroPattern = RegExp(
+  `/${MacroSymbolCharacters}(?:${FormPatternStr})?`,
+  "g"
+);
+
+/**
+ * This pattern matches **anything** between unnested brackets.
+ *
+ * Refoyl encapsulates data that breaks with the Form and MacroSymbol syntax
+ * between brackets. The type of data between brackets can be identified
+ * by the first word used within the brackets.
+ */
+const BracketedPattern = RegExp(bracketed(`[^\[\]]+`), "g");
+/**
+ * These are the data types Refoyl encodes between brackets.
+ * They may be extended in the future.
+ *
+ * There's some overlap with data encoded using `MacroSymbols` some of these
+ * seem to have superceded their symbol versions over time.
+ */
+enum BracketedDataType {
+  // Word-Class Markers
+  Article,
+  Adjective,
+  Adverb,
+  Conjunction,
+  Interjection,
+  Numeral,
+  Participle,
+  Preposition,
+  Pronoun,
+  Verb,
+  // Other Grammar
+  GenderMarker,
+  // Morphology
+  Prefix,
+  // Phonetics
+  Pronunciation,
+  // Semantics
+  Definition,
+  // Usage
+  Clause,
+  Connotations,
+  Idiom,
+  Origin,
+  UsageNote, // Encoding is inconsistent between `grammar` and `usage`
+}
+const BracketedDataTokenizer = buildLexer([
+  // Word-Classes
+  [true, /article/, BracketedDataType.Article],
+  [true, /adj.*/, BracketedDataType.Adjective],
+  [true, /adv.*/, BracketedDataType.Adverb],
+  [true, /conj.*/, BracketedDataType.Adverb],
+  [true, /interj/, BracketedDataType.UsageNote],
+  [true, /d/, BracketedDataType.Numeral],
+  [true, /participle/, BracketedDataType.Participle],
+  [true, /prep/, BracketedDataType.Preposition],
+  [true, /pronoun.*/, BracketedDataType.Pronoun],
+  [true, /verb/, BracketedDataType.Verb],
+  // Other Grammar
+  [true, /[mfn][mfn|]*/, BracketedDataType.GenderMarker],
+  // Morphology
+  [true, /prefix/, BracketedDataType.Prefix],
+  // Phonetics
+  [true, /pronunciation: .+/, BracketedDataType.Pronunciation],
+  // Semantics
+  [true, /def: .+/, BracketedDataType.Definition],
+  // Usage
+  [true, /clause/, BracketedDataType.Clause],
+  [true, /connotations: .+/, BracketedDataType.Connotations],
+  [true, /idiom: .+/, BracketedDataType.Idiom],
+  [true, /origin: .+/, BracketedDataType.Origin],
+  [true, /(note|usage|grammar): .+/, BracketedDataType.UsageNote],
+]);
+
+/**
+ * Entries in Refoyl's source files are composed of three main parts:
+ * 1. The `Form` for an entry, possibly including some spelling annotation.
+ * 2. Any `MacroSymbol`s and their arguments, and
+ * 3. More freeform linguistic data, ending with a definition and optionally a comment.
+ *
+ * Subentries are structured similarly, though their headword may be implicit
+ * (generated from their parent entry as a result of a `MacroSymbol`).
+ *
+ * No component is strictly necessary in every entry,
+ * but every entry must contain at least one of them.
+ */
+enum EntryComponent {
+  Form,
+  Symbol,
+  BracketedData,
+  Comment,
+}
+/**
+ * The definitions for `EntryTokenizer` attempt to be as strict as possible,
+ * but may be slightly more general than the component names suggest,
+ * because the extra data is required for further processing.
+ *
+ * For example: the use of `|` to record optional or alternative endings
+ * without actually encoding them as new entries etc.
+ */
+const EntryTokenizer = buildLexer([
+  // Headwords either begin an entry or are preceded by whitespace,
+  // and crucially are not surrounded by brackets.
+  [true, FormPattern, EntryComponent.Form],
+  // Symbol components should match any `MacroSymbol` with an optional argument.
+  [true, MacroPattern, EntryComponent.Symbol],
+  // Bracketed data has different internal syntax, but the syntax to use
+  // can always be identified by the first word it contains.
+  [true, /\[\]/g, EntryComponent.BracketedData],
+]);
