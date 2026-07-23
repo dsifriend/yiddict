b1ffb2fcadf62b4bde404e345e0a1207ca98e4f0
Albert R. González‑Marín <dsifriend@gmail.com>
2026-07-23 01:23:34 -0400

    Moved away from now unnecessary Sass/SCSS syntax

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,51 +1,52 @@
 <script lang="ts">
-	import '@picocss/pico';
+	import '@picocss/pico/css/pico.min.css';
 	import Search from '$lib/Search.svelte';
 
 	let { children } = $props();
 </script>
 
-	<header>
-		<nav>
-			<ul>
-				<li>
-					<a href="/"><h1 id="home">ייִדיקט</h1></a>
-				</li>
-			</ul>
-			<ul>
-				<li style="padding: 0;">
-					<details class="dropdown">
-						<summary style="width: 8.25em; text-align: center;">אינפאָרמאַציע</summary>
-						<ul>
-							<li class="secondary"><a href="/about">וועגן אונדז</a></li>
-							<li class="secondary"><a href="/help">הילף</a></li>
-							<li class="secondary"><a href="/license">מחברים־רעכט</a></li>
-						</ul>
-					</details>
-				</li>
-			</ul>
-		</nav>
-		<Search></Search>
-	</header>
-	{@render children()}
+<header>
+	<nav>
+		<ul>
+			<li>
+				<a href="/"><h1 id="home">ייִדיקט</h1></a>
+			</li>
+		</ul>
+		<ul>
+			<li style="padding: 0;">
+				<details class="dropdown">
+					<summary style="width: 8.25em; text-align: center;">אינפאָרמאַציע</summary>
+					<ul>
+						<li class="secondary"><a href="/about">וועגן אונדז</a></li>
+						<li class="secondary"><a href="/help">הילף</a></li>
+						<li class="secondary"><a href="/license">מחברים־רעכט</a></li>
+					</ul>
+				</details>
+			</li>
+		</ul>
+	</nav>
+	<Search></Search>
+</header>
+{@render children()}
 
-<style lang="scss">
-	@import '@picocss/pico/scss/colors' ;
 
+<style>
 	* {
 		font-family: Arial, sans-serif;
 	}
 
-	body {
+	:global(body) {
 		padding: 3mm;
-		max-width: 48em;
+		margin-inline: 0 auto;
+	}
 
-		* {
-            max-width: unset;
-		}
+	header,
+	:global(main) {
+		max-width: 48em;
+		margin-inline: auto;
 	}
 
 	#home {
-		color: $azure-500;
+		color: var(--pico-azure-500);
 	}
 </style>
