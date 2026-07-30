eb977127426c3cb9159488ae9790056ca59514d0
Albert R. González‑Marín <dsifriend@gmail.com>
2026-07-29 23:32:29 -0400

    refactor: renamed variables for clarity when adding raw dictionary "fetch"

--- packages/data/src/sources/finkel/fetch.ts
+++ packages/data/src/sources/finkel/fetch.ts
@@ -4,23 +4,23 @@
  * May also fetch raw dictionary file in the future.
  */
 
-const FILENAME = "wordlist.csv";
-const UPSTREAM_URL = `https://www.cs.uky.edu/~raphael/yiddish/${FILENAME}`;
-const OUTPUT_PATH = new URL(`./upstream/${FILENAME}`, import.meta.url);
+const WORDLIST_FILENAME = "wordlist.csv";
+const WORDLIST_UPSTREAM_URL = `https://www.cs.uky.edu/~raphael/yiddish/${WORDLIST_FILENAME}`;
+const WORDLIST_OUTPUT_PATH = new URL(`./upstream/${WORDLIST_FILENAME}`, import.meta.url);
 
 async function main() {
-	const response = await fetch(UPSTREAM_URL);
+	const response = await fetch(WORDLIST_UPSTREAM_URL);
 
 	if (!response.ok) {
 		throw new Error(
-			`Failed to fetch ${UPSTREAM_URL}: ${response.status} ${response.statusText}`,
+			`Failed to fetch ${WORDLIST_UPSTREAM_URL}: ${response.status} ${response.statusText}`,
 		);
 	}
 
 	// Workaround for https://github.com/oven-sh/bun/issues/13237
 	const body = await response.arrayBuffer();
-	await Bun.write(OUTPUT_PATH, body);
-	console.log(`Wrote ${OUTPUT_PATH.pathname}`);
+	await Bun.write(WORDLIST_OUTPUT_PATH, body);
+	console.log(`Wrote ${WORDLIST_OUTPUT_PATH.pathname}`);
 }
 
 await main();
